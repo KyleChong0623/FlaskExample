9400a4267324da23eb8cfb83d405f193b6d6df89
KyleChong0623 <sinyuen0106@gmail.com>
2024-09-16 14:30:21 +0000

    sign up page

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 
 app=Flask(__name__)
 app.config["SECRET_KEY"] = "FoulTarnished"
+
+##routing
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 
 
 ##########
